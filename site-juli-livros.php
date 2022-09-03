2489c6bd26c03054bfe6ac2dfdff460653f15d99
Jessica <88469640+Jess24C@users.noreply.github.com>
2022-09-03 20:44:52 -0300

    Add files via upload

---
+++ site-juli-livros.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> JULIAN ROMERO </title> 
+	<link rel="stylesheet" type="text/css" href="css/site-juli-index.css"> 
+	<style> 
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap');
+</style> 
+</head> 
+<body> 
+<div class="container">
+	<div class="logo"> 
+		<a href="site-juli.php"><img src="/site/img/logo-juli.jpg" width="300" height="300" align="left"></a>
+	</div>
+
+	<div class="navbar" width="100">
+		<a href="site-juli-index.php"><img src="/site/img/home-july.png"></a> &nbsp; &nbsp;
+		<a href="site-juli-livros.php"><img src="/site/img/livros-july.png"></a> &nbsp; &nbsp; 
+		<a href="site-juli-indicacao.php"><img src="/site/img/indicacao-july.png"></a> &nbsp; &nbsp; 
+		<a href="mailto:jessyncard24@gmail.com"><img src="/site/img/contato-july.png"></a> &nbsp; &nbsp;
+	</div>
+
+	<div class="textos">
+		<h3><b>
+			<p> WELCOME TO OUR WEBSITE </p>
+			<p> INSIRA UM TEXTO SOBRE OS LIVROS AQUI </p>
+		</b></h3>
+	</div>
+
+	<div class="rodape">
+			<h2><b> © 2022 JC </b></h2>
+	</div>
+</div>
---
+++ site-juli.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> JULIAN ROMERO </title> 
+	<link rel="stylesheet" type="text/css" href="css/site-juli.css"> 
+	<style> 
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap');
+</style> 
+</head> 
+<body> 
+	<div class="logo"> 
+		<img src="/site/img/logo-juli.jpg">
+	<br></br>
+	<br></br>
+		<a href="site-juli-index.php"><img src="/site/img/iniciar.jpg" width="300" height="80" align="middle"></a>
+	<br></br>
+	 <h2><b>INSCREVA-SE</b></h2>
+	</div>
+	<audio autoplay loop>
+		<source src="/site/sons/reflections-site-juli.mp3" type="audio/mp3">
+		<p> Seu browser não tem suporte a tag audio <p>
+	</audio>
+
+</body> 
+</html> 
